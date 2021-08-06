bdd3dee0fdf07724d0d05f7473e2bb5f833451ad
Erez Ben Moshe <erezbmp@gmail.com>
2021-08-06 05:43:57 +0300

    Use mfilter instead of find

--- src/Chess/Core/MoveConstraints/LeapOver.hs
+++ src/Chess/Core/MoveConstraints/LeapOver.hs
@@ -2,6 +2,7 @@ module Chess.Core.MoveConstraints.LeapOver (leapOverMC) where
 
 import Chess.Core.Models
 import Chess.Core.MoveConstraint
+import Control.Monad
 import Data.List
 import Data.Maybe
 import Utils
@@ -12,7 +13,7 @@ leapOverMC board piece@(Piece piecePlayer pieceType) source =
       isEmpty square = isNothing $ getPiece square board
       go = flip intersect $ do
         offset <- squareOffsets
-        let n = length $ generateList (find isEmpty . applySquareOffset offset) source
+        let n = length $ generateList (mfilter isEmpty . applySquareOffset offset) source
         let squares = generateList (applySquareOffset offset) source
         take (n + 1) squares
    in leapOver repeatability go
